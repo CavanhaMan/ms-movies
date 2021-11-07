4a0221511980e10bca0a42531f1a2fc57254f692
cavanhaman <cavanhaman@hotmail.com>
2021-11-07 01:55:20 -0300

    Add Beam-Autowired para criacao de instancia unica

--- movie-catalog-service/src/main/java/com/iftm/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/iftm/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,9 +2,24 @@ package com.iftm.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
+	
+	//precisamos criar uma unica instancia (singleton) que irá responder às classes
+	//para isso criaremos um bean
+	//a ideia é que sempre  precisar usar esse RestTemplete, vai usar essa instancia
+	//sempre ele - executado uma única vez (não cria diversas instancias durante a execução!)
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+	//o Autowired da classe CatalogResource informa ao Spring que em algum lugar (aqui)
+	//existe um BEAN desse RestTemplate e o inject precisa desse dado e é injetado lá na CatalogResource
+	//o Bean informa que tem algum dado disponível e o Autowired informa que precisa desse dado
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogServiceApplication.class, args);
--- movie-catalog-service/src/main/java/com/iftm/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/com/iftm/moviecatalogservice/resources/CatalogResource.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,13 +18,19 @@ import com.iftm.moviecatalogservice.models.Rating;
 @RestController
 @RequestMapping("/catalog")
 public class CatalogResource {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	//o Autowired aqui informa ao Spring que em algum lugar (MovieCatalogServiceApplication)
+	//existe um BEAN desse RestTemplate e o inject precisa desse dado e é injetado aqui
+	//o Bean informa que tem algum dado disponível e o Autowired informa que precisa desse dado
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
         //return Collections.singletonList(new CatalogItem("Transformers", "Filme de Robo", 4));
         //esse retorno acima foi criado para ele retornar algo "a força" sendo que ainda não temos dados
     	
-    	RestTemplate restTemplate = new RestTemplate();
+    	//RestTemplate restTemplate = new RestTemplate();//como ja temos o restTemplate com Autowired acima, não precisaremos mais desta linha aqui
     	//restTemplat.getForObject("http://localhost:8082/movies/foo", Movie.class);
     	//Sobre o restTemplate:
     	//o primeiro argumento faz uma chamada para o que vc quer chamar via rest
